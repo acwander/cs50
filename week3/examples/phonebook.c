f0be74f5a616a2960f6d2ef3049a76ff5653a004
acwander <50432870+acwander@users.noreply.github.com>
2021-03-16 13:50:50 -0500

    Added constant variable for people array

--- week3/examples/phonebook.c
+++ week3/examples/phonebook.c
@@ -10,16 +10,25 @@ typedef struct
 }
 person;
 
+// declare constant for number of people
+const int NUMBER = 10;
+
 int main(void)
 {
     // initialize people array
-    person people[2];
+    person people[NUMBER];
 
-    people[0].name = "Brian";
-    people[0].number = "+1-617-495-1000";
+    for (int i = 0; i < NUMBER; i++)
+    {
+        people[i].name = get_string("What's the name?");
+        people[i].number = get_string("What's their number?");
+    }
 
-    people[1].name = "David";
-    people[1].number = "+1-949-468-2750";
+    // people[0].name = "Brian";
+    // people[0].number = "+1-617-495-1000";
+
+    // people[1].name = "David";
+    // people[1].number = "+1-949-468-2750";
 
     for (int i = 0; i < 2; i++)
     {
